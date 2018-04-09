a9b459505e323513b7875f77ac6e76de13d4a8b4
Perdog <jacob_perry@hotmail.ca>
2018-04-09 11:07:14 -0400

    Init

---
+++ main.js
@@ -0,0 +1,9 @@
+$document.onLoad(){
+	/*
+	Check for url search
+	If does not exist, load index page
+	If it does, load view page
+	Check for cache, if exists, load it.
+	If load page has never been cached, begin fetching and parsing, show loading page because this could take a while. Once it's done, create a cache.
+	*/
+}
