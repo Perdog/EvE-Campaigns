bd32c49ee4569659641a1edf9b2ab131123449bf
Perdog <jacob_perry@hotmail.ca>
2018-04-17 16:16:33 -0400

    Updates
    - Better spacing
    - Systems listed and sorted by kills
    - Clean up of JS
    - Team names added
    - Error catch if servers are down
    - Loading page is seeeexxxyyyyy
    - Cleaned up old logging

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-var testData = [];
-
-var allIDs = [];
-var teamIDs = [];
+// Search query stuff
+var allIDs = {};
 var aIDs = [];
 var bIDs = [];
 var dates = [];
+
+// Kills stuff
+var allKills = [];
+var waitingOn = 0;
+var pageNumber = 0;
+var keepSearching = false;
+
+// Stats stuff
+var totalKills = [0,0];
+var totalValues = [0,0];
+var systems = [];
 
 $(document).ready(function(){
 	/*
@@ -28,12 +37,10 @@ $(document).ready(function(){
 		aIDs = teamA.split(",");
 		bIDs = teamB.split(",");
 		
-		allIDs = aIDs.concat(bIDs);
+		$('#loading-page').show(500);
 		
-		console.log("All IDs\n" + allIDs);
-		
-		$('#loading-page').show();
-		getNextPage();
+		// TODO Check for cache here
+		getNames(aIDs.concat(bIDs));
 		
 	} else {
 		// If not, load normal page
@@ -42,12 +49,43 @@ $(document).ready(function(){
 	}
 });
 
-var allKills = [];
-var waitingOn = 0;
-var pageNumber = 0;
-var keepSearching = false;
+function getNames(list) {
+	$("#load-text").text("Attempting to load team names...");
+	
+	var url = "https://esi.tech.ccp.is/latest/universe/names/?datasource=tranquility";
+	var fetch = new XMLHttpRequest();
+	fetch.onload = parseTeamNames;
+	fetch.onerror = reqerror;
+	fetch.open('post', url, true);
+	fetch.setRequestHeader('Content-Type','application/json');
+	fetch.setRequestHeader('accept','application/json');
+	fetch.send(JSON.stringify(list));
+}
+
+function parseTeamNames() {
+	var data = JSON.parse(this.responseText);
+	
+	if (data.error) {
+		esiError(data.error);
+		$("#load-text").text("Failed to load team names...");
+	} else {
+		$("#load-text").text("Names found...");
+		for (var i = 0; i < data.length; i++) {
+			var temp = {};
+			temp.name = data[i].name;
+			temp.type = data[i].category;
+			allIDs[data[i].id] = temp;
+		}
+		
+		getNextPage();
+	}
+}
+
 function getNextPage() {
-	var howMany = Math.max(20/allIDs.length, 1);
+	console.log("Trying to fetch kills");
+	$("#load-text").text("Fetching some killmails...");
+	var idLength = Object.keys(allIDs).length;
+	var howMany = Math.max(20/idLength, 1);
 	for (var h = 0; h < howMany; h++) {
 		pageNumber++;
 		var base = "https://zkillboard.com/api/kills/";
@@ -58,15 +96,16 @@ function getNextPage() {
 		var page = "/no-items/page/";
 		var dontForgetThis = "/";
 		
-		for (var i = 0; i < allIDs.length; i++) {
+		for (var i = 0; i < idLength; i++) {
 			waitingOn++;
-			var zurl = base + (allIDs[i][0] == "A" ? alli + allIDs[i].substring(1) : corp + allIDs[i].substring(1)) + sTime + dates[0] + eTime + dates[1] + page + pageNumber + dontForgetThis;
-			console.log(zurl);
+			var id = Object.keys(allIDs)[i];
+			var zurl = base + (allIDs[id].type == "alliance" ? alli + id : corp + id) + sTime + dates[0] + eTime + dates[1] + page + pageNumber + dontForgetThis;
 			
 			var fetch = new XMLHttpRequest();
 			fetch.onload = reqsuc;
 			fetch.onerror = reqerror;
 			fetch.open('get', zurl, true);
+			//fetch.setRequestHeader('Requester-Info','EvE Campaigns --- Maintainer: Pedro Athonille @ jacob_perry@hotmail.ca');
 			fetch.send();
 		}
 	}
@@ -75,108 +114,209 @@ function getNextPage() {
 function reqsuc() {
 	var data = JSON.parse(this.responseText);
 	
-	if (data.length > 0)
+	if (data.length > 1)
 		keepSearching = true;
+	
+	$("#load-text").text("Parsing kill data...");
 	
 	// Lets do something with the data
 	for (var i = 0; i < data.length; i++) {
-		// First, we'll grab the victims corp and alliance IDs (We don't know if people only want one corp from an alliance so need to check both)
-		var vAlli = "A";
-		var vCorp = "C";
-		if (data[i].victim.alliance_id)
-			vAlli += data[i].victim.alliance_id;
-		if (data[i].victim.corporation_id)
-			vCorp += data[i].victim.corporation_id;
-		// Then we make sure the victim is in our list of wanted IDs (Checking all of them since we don't know which fetch call this came from)
-		if (allIDs.includes(vAlli) || allIDs.includes(vCorp)) {
+		// Make sure the victim is in our list of wanted IDs (Checking all of them since we don't know which fetch call this came from)
+		if (Object.keys(allIDs).includes(data[i].victim.alliance_id + "") || Object.keys(allIDs).includes(data[i].victim.corporation_id + "")) {
 			// If either true, we want to keep this data
 			console.log("Keeping one");
 			allKills.push(data[i]);
+			
+			// STATS COLLECTION
+			// Used for the arrays. Defaulting to -1 for error catching. Should never see it, but juuuuuust in case.
+			var team = getTeam(data[i].victim);
+			if (team >= 0) {
+				// Track total kills
+				totalKills[team] += 1;
+				
+				// Track total kill values
+				totalValues[team] += data[i].zkb.totalValue;
+				
+				// Track kills per ship per team
+					// Topkeks that's gonna be fun
+				
+				// Track kills per system per team
+				if (!systems[team])
+					systems[team] = {};
+				if (!systems[team][data[i].solar_system_id]) {
+					systems[team][data[i].solar_system_id] = {};
+					systems[team][data[i].solar_system_id].kills = 0;
+				}
+				systems[team][data[i].solar_system_id].kills += 1;
+			}
 		}
 	}
-	// If this is the last fetch, build the webpage
+	// If this is the last fetch, and there was no data on the page, build the webpage
 	waitingOn--;
 	if (waitingOn == 0)
 		if (keepSearching) {
 			keepSearching = false;
+			$("#load-text").text("Done parsing this page, next...");
 			getNextPage();
+		} else {
+			$("#load-text").text("All possible kills found...");
+			doneFetchingKills();
 		}
-		else
-			allRequestsDone();
 }
 
 function reqerror(error) {
-	console.log("Oh no! Something's wrong!\nPing Pedro on Discord and give his this error code:\n" + error);
+	console.log("Oh no! Something's wrong!\n" + error);
 }
 
-function allRequestsDone() {
-	console.log("Everything is done");
+function doneFetchingKills() {
+	console.log("Kill fetch is done");
+	// TODO cache this request here
 	
-	pullStats();
+	// Sort kills by date
+	allKills.sort(by('killmail_time'));
+	
+	// Do system name lookups
+	loadSystemNames();
 }
 
-// Stats stuff
-var totalKills = [0,0];
-var totalValues = [0,0];
-var systems = [];
+function loadSystemNames() {
+	console.log("Fetching system names");
+	$("#load-text").text("Loading system names...");
+	
+	var list = [];
+	
+	for (var i = 0; i < systems.length; i++) {
+		Object.keys(systems[i]).forEach(function(key) {
+			if (!list.includes(key))
+				list.push(key);
+		});
+	}
+	
+	var url = "https://esi.tech.ccp.is/latest/universe/names/?datasource=tranquility";
+	var fetch = new XMLHttpRequest();
+	fetch.onload = parseSystems;
+	fetch.onerror = reqerror;
+	fetch.open('post', url, true);
+	fetch.setRequestHeader('Content-Type','application/json');
+	fetch.setRequestHeader('accept','application/json');
+	fetch.send(JSON.stringify(list));
+}
+
+function parseSystems() {
+	var data = JSON.parse(this.responseText);
+	
+	if (data.error) {
+		esiError(data.error);
+		$("#load-text").text("Error loading system names...");
+	} else {
+		data.forEach(function(key) {
+			for (var i = 0; i < systems.length; i++) {
+				if (Object.keys(systems[i]).includes(key.id+"")) {
+					systems[i][key.id+""].name = key.name;
+				}
+			}
+		});
+		
+		$("#load-text").text("System names loaded...");
+		pullStats();
+	}
+}
 
 function pullStats() {
-	allKills.sort(by('killmail_time'));
-	console.log(allKills);
+	$("#load-text").text("Parsing all this info...");
 	
-	for (var i = 0; i < allKills.length; i++) {
-		// Used for the arrays. Defaulting to -1 for error catching
-		var team = -1;
-		// Is a kill for Team A
-		if (isForTeam(1, allKills[i].victim))
-			team = 0;
-		// Is a kill for Team B
-		else if (isForTeam(2, allKills[i].victim))
-			team = 1;
-		
-		if (team >= 0) {
-			// Track total kills
-			totalKills[team] += 1;
-			// Track total kill values
-			totalValues[team] += allKills[i].zkb.totalValue;
-			// Track kills per system per team
-			if (!systems[team])
-				systems[team] = {};
-			if (!systems[team][allKills[i].solar_system_id])
-				systems[team][allKills[i].solar_system_id] = 0;
-			systems[team][allKills[i].solar_system_id] += 1;
-			// Track kills per ship per team
-				// Topkeks that's gonna be fun
-			
-		}
-	}
+	//for (var i = 0; i < allKills.length; i++) {}
+	
+	// Set names. We loop through allIDs and compare it to the teamIDs, attaching them to the appropriate string.
+	var aTeamNames = "";
+	var bTeamNames = "";
+	Object.keys(allIDs).forEach(function(key) {
+		if (aIDs.includes(key))
+			aTeamNames += allIDs[key].name + "\n";
+		else if (bIDs.includes(key))
+			bTeamNames += allIDs[key].name + "\n";
+	});
+	$('#TeamA').find('#names').text(aTeamNames);
+	$('#TeamB').find('#names').text(bTeamNames);
+	
+	// Replace kill count
 	$('#TeamA').find('#kills').text(totalKills[0].toLocaleString(undefined, {maximumFractionDigits:2}));
 	$('#TeamB').find('#kills').text(totalKills[1].toLocaleString(undefined, {maximumFractionDigits:2}));
+	
+	// Replace isk values
 	$('#TeamA').find('#value').text(totalValues[0].toLocaleString(undefined, {maximumFractionDigits:2}));
 	$('#TeamB').find('#value').text(totalValues[1].toLocaleString(undefined, {maximumFractionDigits:2}));
 	
-	
-	$('#campaign-page').show();
-	
-	
-	console.log(totalKills);
-	console.log(totalValues);
-	console.log(systems);
+	// Set system names
+	var aTeamSystems = "";//Object.keys(systems[0]).toString().replace(",","\n");
+	var bTeamSystems = "";
+	for (var i = 0; i < systems.length; i++) {
+		Object.values(systems[i]).forEach(function(v) {
+			if (i == 0)
+				aTeamSystems += "<tr><td>" + v.name + "</td><td>" + v.kills + "</td></tr>";//v.name + (v.name.length >= 8 ? "\t" : v.name.length >= 4 ? "\t\t" : v.name.length < 4 ? "\t\t\t" : "") + v.kills + "\n";
+			else
+				bTeamSystems += "<tr><td>" + v.name + "</td><td>" + v.kills + "</td></tr>";//v.name + (v.name.length >= 8 ? "\t" : v.name.length >= 4 ? "\t\t" : v.name.length < 4 ? "\t\t\t" : "") + v.kills + "\n";
+		});
+	}
+	$('#TeamA').find('#systems').append(aTeamSystems);
+	$('#TeamB').find('#systems').append(bTeamSystems);
+	sortTables();
+	$("#load-text").text("Ready to go");
+	$('#loading-page').hide(600);
+	$('#campaign-page').show(600);
 }
 
-function isForTeam(team, victim) {
-	var found = false;
-	// We do this backwards because we're checking victim, not attacker.
-	var ids = (team == 1 ? bIDs : aIDs);
+function sortTables() {
+	var table, rows, switching, i, x, y, shouldSwitch;
 	
-	if (victim.alliance_id) {
-		if (ids.includes("A" + victim.alliance_id) || ids.includes("C" + victim.corporation_id))
-			found = true;
+	for (var h = 0; h < systems.length; h++) {
+		table = (h == 0 ? document.getElementById("TeamA") : document.getElementById("TeamB"));
+		switching = true;
+		/* Make a loop that will continue until
+		no switching has been done: */
+		while (switching) {
+			// Start by saying: no switching is done:
+			switching = false;
+			rows = table.getElementsByTagName("TR");
+			// Loop through all table rows:
+			for (i = 0; i < (rows.length - 1); i++) {
+				// Start by saying there should be no switching:
+				shouldSwitch = false;
+				/* Get the two elements you want to compare,
+				one from current row and one from the next: */
+				x = rows[i].getElementsByTagName("TD")[1];
+				y = rows[i + 1].getElementsByTagName("TD")[1];
+				/* Check if the two rows should switch place,
+				based on the direction, asc or desc: */
+				if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+					// If so, mark as a switch and break the loop:
+					shouldSwitch= true;
+					break;
+				}
+			}
+			if (shouldSwitch) {
+				/* If a switch has been marked, make the switch
+				and mark that a switch has been done: */
+				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+				switching = true;
+			}
+		}
 	}
-	else if (ids.includes("C" + victim.corporation_id))
-		found = true;
+}
+
+function getTeam(victim) {
+	if (victim.alliance_id) {
+		if (aIDs.includes(victim.alliance_id+"") || aIDs.includes(victim.corporation_id+""))
+			return 1;
+		else if (bIDs.includes(victim.alliance_id+"") || bIDs.includes(victim.corporation_id+""))
+			return 0;
+	}
+	else if (aIDs.includes(victim.corporation_id+""))
+		return 1;
+	else if (bIDs.includes(victim.corporation_id+""))
+		return 0;
 	
-	return found;
+	return -1;
 }
 
 var by = function (path, reverse, primer, then) {
@@ -205,6 +345,10 @@ var by = function (path, reverse, primer, then) {
         ) * [1,-1][+!!reverse];
     };
 };
+
+function esiError(error) {
+	alert("Failed to load ESI data. CCP servers might be having some troubles, or it could be downtime.\n\nError received: " + error);
+}
 
 function parseSearch(variable) {
     var query = window.location.search.substring(1);
